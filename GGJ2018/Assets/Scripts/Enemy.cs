80b1766c7f7ea442aee25de1d37c0553fab69ace
Kanabenki <31719392+Kanabenki@users.noreply.github.com>
2018-01-26 23:00:10 +0100

    Add basic enemy script

---
+++ GGJ2018/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	public float moveSpeed = 5.0f;
+	public float turnSpeed = 1.0f;
+	public string targetTag = "Player";
+	private GameObject[] players;
+
+	private Transform target = null;
+	private Rigidbody rb = null;
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+		players = GameObject.FindGameObjectsWithTag(targetTag);
+		target = GetClosestPlayer(players).transform;
+		}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		target = GetClosestPlayer(players).transform;
+		transform.right = target.position - transform.position;
+		if (Vector3.Distance(transform.position, target.position) > Mathf.Epsilon) {
+			rb.velocity = transform.right * moveSpeed;
+		} else {
+			rb.velocity = new Vector3(0,0,0);
+		}
+	}
+
+	GameObject GetClosestPlayer(GameObject[] players) {
+		GameObject closest = null;
+		var closestDstSqr = Mathf.Infinity;
+		var currPosition = transform.position;
+		foreach (var player in players) {
+			var dstToPlayer = player.transform.position - currPosition;
+			if (dstToPlayer.sqrMagnitude < closestDstSqr) {
+				closestDstSqr = dstToPlayer.sqrMagnitude;
+				closest = player;
+			}
+		}
+		return closest;
+	}
+}
