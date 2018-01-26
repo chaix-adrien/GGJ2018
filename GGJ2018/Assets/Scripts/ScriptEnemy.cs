687bda889d6a044d2e45f80da31a3c3fdd5b8335
Kanabenki <31719392+Kanabenki@users.noreply.github.com>
2018-01-26 23:30:38 +0100

    Fix enemy script name

--- GGJ2018/Assets/Scripts/ScriptEnemy.cs
+++ GGJ2018/Assets/Scripts/ScriptEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class ScriptEnemy : MonoBehaviour {
 
 	// Use this for initialization
 	public float moveSpeed = 5.0f;
