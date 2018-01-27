512bc62f2bd05d69926a90f78459679ac6587c2d
naimatsu <19470393+naimatsu@users.noreply.github.com>
2018-01-27 19:49:19 +0100

    PathFinding Improvements #1

--- GGJ2018/Assets/Scripts/ScriptPathFinding.cs
+++ GGJ2018/Assets/Scripts/ScriptPathFinding.cs
@@ -22,6 +22,33 @@ public class ScriptPathFinding : MonoBehaviour {
 		}
     }	
 
+    void FixedUpdate()
+    {
+        List <float> angles = new List<float>();
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        float closest = 50;
+        for(int i = -45; i < 45; i+=5)
+        {
+            if(!Physics.Raycast(transform.position, new Vector3(rb.transform.rotation.x, rb.transform.rotation.y, rb.transform.rotation.z + i),5))
+            {
+                angles.Add(i);
+            }
+        }
+
+        foreach(float angle in angles)
+        {
+            if(Mathf.Abs(angle) < Mathf.Abs(closest))
+            {
+                closest = angle;
+            }
+        }
+        
+        // The gameobject is oriented at the closest angle
+        GetComponent<Rigidbody>().transform.Rotate(0, 0, closest);     
+
+
+    }
+
 	void OnCollisionEnter (Collision col)
     {
         if(col.gameObject.tag == "FullFloor")
