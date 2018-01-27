a9ae6056cae3e039c1e353fb769e74845a6fbc56
naimatsu <19470393+naimatsu@users.noreply.github.com>
2018-01-27 01:15:44 +0100

    Modification Spawn Enemy Script

--- GGJ2018/Assets/Scripts/ScriptSpawnEnemy.cs
+++ GGJ2018/Assets/Scripts/ScriptSpawnEnemy.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class ScriptSpawnEnemy : MonoBehaviour {
 
+	public GameObject enemy;
+	public float spawnTime;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		InvokeRepeating ("SpawnEnemy", spawnTime, spawnTime);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
+	}
+
+	void SpawnEnemy()
+	{
+		//Instantiate(enemy,new Vector3(,0,0), Quaternion.identity);
 	}
 }
