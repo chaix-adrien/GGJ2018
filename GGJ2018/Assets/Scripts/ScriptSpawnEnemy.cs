63ce3ecbfcdf8eadb4107a54487be4a34f1e2f62
naimatsu <19470393+naimatsu@users.noreply.github.com>
2018-01-27 01:48:49 +0100

    Modification Spawn Enemy Script

--- GGJ2018/Assets/Scripts/ScriptSpawnEnemy.cs
+++ GGJ2018/Assets/Scripts/ScriptSpawnEnemy.cs
@@ -2,24 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptSpawnEnemy : MonoBehaviour {
+public class ScriptSpawnEnemy : MonoBehaviour
+{
 
-	public GameObject enemy;
-	public float spawnTime;
-	// Use this for initialization
-	void Start ()
-	{
-		InvokeRepeating ("SpawnEnemy", spawnTime, spawnTime);
-	}
+    public GameObject enemy;
+    public float spawnTime;
+	public int numberOfEnemy;
+    private float spawnX;
+    private float spawnY;
+	private Vector3 spawnPosition;
+	Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+		cam = GetComponent<Camera>();
+        InvokeRepeating("SpawnEnemy", spawnTime, spawnTime);
+    }
 
-	// Update is called once per frame
-	void Update ()
-	{
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
 
-	void SpawnEnemy()
-	{
-		//Instantiate(enemy,new Vector3(,0,0), Quaternion.identity);
-	}
+    void SpawnEnemy()
+    {
+        Vector3 dir;
+        Vector3 position;
+
+		spawnPosition.x = Random.Range (-17, 17);
+        spawnPosition.y = Random.Range (-17, 17);
+        spawnPosition.z = 0.0f;
+
+		// Spawn an amount of enemies (numberOfEnemy) randomly on the map
+		for(int i = 0; i < numberOfEnemy; i++)
+		{
+			dir = Random.insideUnitCircle;
+			position = Vector3.zero;
+
+			// Check if there's a gameobject present on a random position 
+			if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+			{//make it appear on the side
+				position = new Vector3(Mathf.Sign(dir.x) * Camera.main.orthographicSize * Camera.main.aspect,
+										0,
+										dir.y * Camera.main.orthographicSize);
+			}
+			else
+			{//make it appear on the top/bottom
+				position = new Vector3(dir.x * Camera.main.orthographicSize * Camera.main.aspect,
+										0,
+										Mathf.Sign(dir.y) * Camera.main.orthographicSize);
+			}
+			Instantiate(enemy, position, Quaternion.identity);
+		}
+       
+    }
 }
