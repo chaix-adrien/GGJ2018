7d79fbe08d9209afac79e9bfbcacfe29d83b2d0e
adrien chaix <adrien.chaix@epitech.eu>
2018-01-28 14:43:57 +0100

    QuitButton

---
+++ GGJ2018/Assets/ScriptButtonExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptButtonExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(quitGame);
+	}
+	
+	void quitGame() {
+		Application.Quit();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
